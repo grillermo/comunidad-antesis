9f138c588ae9d30bc3de5dc228f5684a271e42ec
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-07-10 00:04:48 -0600

    Seed admin from env, document ADMIN_* vars, note propshaft admin assets

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if ENV["ADMIN_EMAIL"].present? && ENV["ADMIN_PASSWORD"].present?
+  User.find_or_create_by!(email: ENV.fetch("ADMIN_EMAIL")) do |u|
+    u.password = ENV.fetch("ADMIN_PASSWORD")
+    u.role = :admin
+  end
+  puts "Seeded admin #{ENV.fetch('ADMIN_EMAIL')}"
+else
+  puts "Skipping admin seed (set ADMIN_EMAIL and ADMIN_PASSWORD to enable)"
+end
