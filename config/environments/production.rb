60af5751136031043881185caec57dd91b993465
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-07-13 16:40:36 -0600

    Ensure we can serve from localhost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,10 +75,12 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "comunidad.antesis.com.mx",     # Allow requests from example.com
+    "0.0.0.0",     # Allow requests from example.com
+    "192.168.1.1",     # Allow requests from example.com
+    "localhost"
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
